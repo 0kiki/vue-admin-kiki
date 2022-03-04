d27e565d91dab8be337f9ad45985c6476d69d3fd
0kiki <272671898@qq.com>
2022-03-04 21:00:20 +0800

    form

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,16 +1,16 @@
 <!--  -->
 <template>
   <div>
-    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+    <el-form :inline="true" :model="formInline.template" :rules="rules" class="demo-form-inline">
       <el-row>
         <el-col :span="8">
-          <el-form-item label="审批人">
-            <el-input v-model="formInline.user" placeholder="审批人" />
+          <el-form-item label="审批人" prop="user">
+            <el-input v-model="formInline.template.user" placeholder="审批人" />
           </el-form-item>
         </el-col>
         <el-col :span="8">
-          <el-form-item label="活动区域">
-            <el-select v-model="formInline.region" placeholder="活动区域">
+          <el-form-item label="活动区域" prop="region">
+            <el-select v-model="formInline.template.region" placeholder="活动区域">
               <el-option label="区域一" value="shanghai" />
               <el-option label="区域二" value="beijing" />
             </el-select>
@@ -32,9 +32,23 @@ export default {
   data() {
     return {
       formInline: {
-        user: '',
-        region: ''
+        pageNum: 1,
+        pageSize: 10,
+        template: {
+          user: '',
+          region: ''
+        }
+      },
+      rules: {
+        user: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        region: [
+          { required: true, message: '请选择活动区域', trigger: 'change' }
+        ]
       }
+
     }
   },
   // 生命周期 - 创建完成（访问当前this实例）
